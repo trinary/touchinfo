b98063c9dc4ed14fb639093e5e792a81494b8830
Erik Cunningham <djtrinary@gmail.com>
2015-07-24 17:26:28 -0600

    dsfasdf

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 
 var svg = d3.select("body").append("svg").attr("id", "main");
-var text = svg.append("text").attr({
+var text = svg.append("text").classed("touches", true).attr({
   dx: 200,
   dy: 200
 }).text("touch or click");
+var text = svg.append("text").classed("event", true).attr({
+  dx: 200,
+  dy: 400
+}).text("event?");
 console.log(text);
 
 
@@ -11,9 +15,11 @@ svg.on("touchstart", printTouches);
 svg.on("touchmove", printTouches);
 svg.on("touchend", printTouches);
 svg.on("mousedown", printTouches);
+svg.on("mouseup", printTouches);
 
-function printTouches() { 
-  svg.select("text").text(JSON.stringify(d3.touches(svg)));
+function printTouches(event) { 
+  svg.select(".touches").text(JSON.stringify(d3.touches(svg)));
+  svg.select(".event").text(JSON.stringify(event));
 
   requestAnimationFrame(printTouches);
 }
